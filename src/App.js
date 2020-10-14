73b3b325d7b1cfe06086d74469b5a1050683b481
Mark Evans <markthedev@gmail.com>
2020-10-14 16:56:04 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './table.css';
 
 function App() {
+  const [starPeeps, setStarPeeps] = useState();
+  const [apiURL, setAPIUrl] = useState('https://swapi.dev/api/people/');
+  const [isPaginated, setIsPaginated] = useState(false);
+  const [prevURL, setPrevURL] = useState(null);
+
+  useEffect(() => {
+    fetchData(apiURL);
+  }, []);
+
+  const fetchData = (apiURL) => {
+    fetch(apiURL)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(jsonResp) {
+      setStarPeeps(jsonResp.results);
+      setAPIUrl(jsonResp.next);
+      setPrevURL(jsonResp.previous);
+    })
+    .catch(function(err) {
+      console.log(err);
+    })
+  }
+
+  const goToPrevPage = () => {
+    let isFirstPage = prevURL.split('=')[1];
+    console.log(isFirstPage);
+    if (isFirstPage !== '1') {
+      fetchData(prevURL);
+    } else {
+      fetchData(apiURL);
+      setIsPaginated(false);
+    }
+  }
+
+  const goToNextPage = () => {
+    setIsPaginated(true);
+    fetchData(apiURL);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Starwars</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Homeworld</th>
+          </tr>
+        </thead>
+        <tbody>
+          {starPeeps && starPeeps.map((person, index) => (
+            <tr key={index} className={index%2 === 0 ? 'even' : 'odd'}>
+              <td>{person.name}</td>
+              <td>{person.gender}</td>
+              <td>{person.homeworld}</td>
+            </tr>
+          ))}
+          <tr>
+            {isPaginated ?
+              <td><button type="button" onClick={goToPrevPage}>Go Back</button></td>
+            :
+              null
+            }
+            <td><button type="button" onClick={goToNextPage}>Click me Obi Wan, you're my only hope</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </>
   );
 }
 
